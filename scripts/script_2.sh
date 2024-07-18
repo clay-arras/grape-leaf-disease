1774f50d95eb7abab9f65ed742e72a3c0b60e58c
Neil Lin <nlin06@student.ubc.ca>
2024-07-18 15:06:34 -0700

    lksjdflksj

--- scripts/script_2.sh
+++ scripts/script_2.sh
@@ -6,23 +6,19 @@
 #SBATCH --job-name=cosine_similarity
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=astrollin.neil@gmail.com
-#SBATCH --mem=48G
+#SBATCH --mem=24G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --output=oCosineSim.txt
 #SBATCH --error=eCosineSim.txt
-#SBATCH --time=16:00:00
+#SBATCH --time=2:00:00
 
 module load gcc python miniconda3 cuda cudnn
 
 source ~/.bashrc
-conda activate grape-ld
 
 cd /scratch/st-sielmann-1/agrobot/grape-ld/data/
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-python run.py
-
-conda deactivate
-
+~/miniconda3/envs/grape-ld/bin/python run.py
